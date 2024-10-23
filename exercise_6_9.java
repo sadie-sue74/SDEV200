609f9a898f93f2d65544fc7a323487067101d3f5
sadie-sue74 <117745010+sadie-sue74@users.noreply.github.com>
2024-10-23 15:53:45 -0400

    Add files via upload

---
+++ exercise_6_9.java
@@ -0,0 +1,26 @@
+public class exercise_6_9 {
+    public static void main(String[] args) {
+        //Display table
+        System.out.println(
+            "\nFeet     Meters        |    Meters        Feet\n" + 
+            "-------------------------------------------------");
+            
+        //For loop to run through printing out the conversion information
+        for(double feet = 1.0, meters = 20.0; feet <= 10.0; feet++, meters += 5)
+        {
+            System.out.printf("%4.1f    ", feet);
+            System.out.printf("%6.3f    ", footToMeter(feet));
+			System.out.printf("     |     ");
+			System.out.printf("%-11.1f", meters);
+			System.out.printf("%7.3f\n", meterToFoot(meters));
+        }
+    }
+
+    public static double footToMeter(double foot){
+        return 0.305 * foot;
+    }
+
+    public static double meterToFoot(double meter){
+        return 3.279 * meter;
+    }
+}
